cbfb7694cffe7d4410511b3ece57623e6875e499
Neil Ryan L <neilryanlipaod@gmail.com>
2021-12-16 17:24:29 +0800

    ADDED all

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React from 'react';
+import { FcAndroidOs } from 'react-icons/fc';
 import {
   ChakraProvider,
-  Box,
+  Flex,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  Button,
+  useDisclosure,
+  Slide
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import '@fontsource/mountains-of-christmas/400.css'
+import '@fontsource/mountains-of-christmas/700.css'
+import theme from './theme';
+import { Image } from '@chakra-ui/react';
 
 function App() {
+  const { isOpen, onToggle } = useDisclosure();
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Flex justifyContent={'center'} minH={'100vh'}  backgroundColor={'#f9c1b1'} bgGradient={'linear-gradient(315deg, #f9c1b1 0%, #fb8085 74%)'}>
+        <Flex flexDir={'column'} justifyContent={'center'} color={'white'} minWidth={[300, 500, 700]}>
+              
+      
+            <Flex>
+              <Image onClick={onToggle} alignSelf={'center'} src="rastaclaus.png" boxSize={['50', '88']}></Image>
+              <Text textAlign={'center'} fontWeight={'extrabold'} fontSize={['6xl','8xl']}>          
+                  Secret Rasta
+                </Text>
+              <Image onClick={onToggle}  alignSelf={'center'} src="rastaclaus.png" boxSize={['50', '88']}></Image>
+            </Flex>
+            <Flex maxWidth={'700px'}>
+              
+              <Text mx={'10'} fontWeight={'bold'} fontSize={['lg', '3xl']}>
+              The true Christmas spirit is putting others' happiness before our own, and finding you've never known such happiness. Make someone's day, be a Secret Rasta.
+              </Text>
+            </Flex>
+            
+            <Button _focus={'none'} mx={['5%','30%']} color={'red.600'} rightIcon={<FcAndroidOs />}>
+              Download Secret Rasta Now
+            </Button>
+            <Slide direction='top' in={isOpen} style={{ zIndex: 10 }}>
+              <Image src='calapecute.png' />
+      
+            </Slide>
+        
+        </Flex>
+       
+      
+      </Flex>
+     
     </ChakraProvider>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Mountains of Christmas',
+    body: 'Mountains of Christmas',
+  },
+})
+
+export default theme
